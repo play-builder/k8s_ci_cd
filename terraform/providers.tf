aa3c8419707d5551eec77447759e0651028418b0
play-builder <playbuilder47@gmail.com>
2024-04-01 21:39:35 +0900

    chore(tf): Configure AWS and Helm providers

---
+++ terraform/providers.tf
@@ -0,0 +1,82 @@
+terraform {
+
+  required_version = ">= 1.5.0"
+
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.30"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.24"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+  }
+
+
+
+
+
+
+
+
+  backend "s3" {
+    bucket = "plydevops-infra-tf-dev"
+    key    = "ci_cd/terraform.tfstate"
+    region = "us-east-1"
+
+    encrypt = true
+
+
+
+  }
+}
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = "k8s-ci-cd"
+      Environment = var.environment
+      ManagedBy   = "terraform"
+      Repository  = "applejjmango/eks_ci_cd"
+    }
+  }
+}
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name", module.eks.cluster_name,
+      "--region", var.aws_region
+    ]
+  }
+}
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name", module.eks.cluster_name,
+        "--region", var.aws_region
+      ]
+    }
+  }
+}
